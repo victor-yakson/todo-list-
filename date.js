342ca36b0eda74df27aec461c4f8f425ced7e03d
unknown <victoryakubu09@gmail.com>
2022-06-24 11:21:41 +0100

    first commit

---
+++ date.js
@@ -0,0 +1,20 @@
+ 
+ exports.getDate = ()=>{
+    let today = new Date();
+    let options={
+        weekday:"long",
+        day: "numeric",
+        month:"long"
+    }
+    let day = today.toLocaleDateString("en-US",options);
+    return day
+};
+exports.getDay =()=>{
+    let today = new Date();
+    let options={
+        weekday:"long",
+       
+    }
+    let day = today.toLocaleDateString("en-US",options);
+    return day
+};
